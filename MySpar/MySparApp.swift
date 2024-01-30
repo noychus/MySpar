4dc016a0dfe322feb5886df8d70a60e1bd999d82
noychus <iconyami@icloud.com>
2024-01-30 14:48:02 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by NOY  on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
